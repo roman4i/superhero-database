a13bac61f7622203a6efa4add7d40c5b641da729
roman4i <verb.r17@gmail.com>
2022-08-03 15:02:16 +0300

    Added Divider component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Title from '../Title/Title';
+import Divider from '../Divider/Divider';
 import './app.css';
 
 function App() {
   return (
     <div className='appBox'>
       <Title />
+      <Divider />
     </div>
   );
 }
